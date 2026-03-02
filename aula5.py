a9b15180d6955eb76e601f162581a62c0ee30411
Richard Marx <bellusci.richard@escola.pr.gov.br>
2026-03-02 09:44:47 -0300

    Update game logic for number guessing
    Refactor game logic to allow more attempts and improve feedback.

--- aula5.py
+++ aula5.py
@@ -1,25 +1,29 @@
 # Importação de biblioteca
 import random
 
-# Sorteio do número aleatório
-numero = random.randint(0, 10)
-
-# TESTE
-print(numero)
-
+# Configurações do jogo
 tentativas = 1
-while (tentativas <= 3):
+errou = True
+sorteio_max = 10
+tentativas_max = 10
+numero = random.randint(0,10)
+
+while (tentativas <= tentativas_max):
     print(f"Tentativa {tentativas}")
     chute = int(input("Digite o seu chute (0 a 10): "))
     if chute == numero:
+        errou = False
         print("Parabéns, você é o bonzão mesmo!")
         break
     else:
         print("Errou :(")
-        if (chute > numero):
-            print(f"O número é menor que {chute}")
-        else:
-            print(f"O número é maior que {chute}")
+        if (tentativas <= (tentativas_max - 1)):
+            if (chute > numero):
+                print(f"O número é menor que {chute}")
+            else:
+                print(f"O número é maior que {chute}")
     tentativas = tentativas + 1
-print(f"O número sorteado era {numero}")
+
+if (errou == True):
+    print(f"O número sorteado era {numero}")
 print("### FIM DO JOGO ###")
