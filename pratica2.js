2ddf07d290cc54ed126a695a084010758c42c5ca
Richard Marx <bellusci.richard@escola.pr.gov.br>
2026-03-04 12:30:04 -0300

    Add files via upload

--- pratica2.js
+++ pratica2.js
@@ -13,17 +13,37 @@ Imagine que você abriu uma loja:
     - mostra estoque
 */
 
-
+// lista de produtos
 const estoque = ["Sonic Unleashed", "Sonic Colors", "Sonic Frontiers", "Sonic 2006", "Sonic Forces",
                  "Sonic 1", "Sonic 2", "Sonic 3 & Knuckles"]
+
+// lista de preços
 const preco = [     "R$ 64,00",      "R$ 143,00",     "R$ 275,00",      "R$ 30,00",   "R$ 120,00",
                  "R$ 7,99", "R$ 7,99",      "R$ 9,99"]
-function mostrarCatalogo(){
+                 
+function mostrarCatalogo(){ // mostra os produtos e seus preços
     let contador = 0 // variável de início
     while(contador < estoque.length){ // condição de parada
-        console.log(`${estoque[contador]}: ${preco[contador]}`)
+        console.log(`${estoque[contador]}: ${preco[contador]}`) // exibição do item
         contador++ // controle do laço
     }
+    console.log("## FIM DA LISTA ##")
 }
-mostrarCatalogo()
 
+function addProduto(addProduto, addPreco){ // adiciona um item ao final da lista
+    estoque.push(addProduto) // adiciona produto
+    preco.push(addPreco) // adiciona preço
+    console.log(`Você adicionou ${addProduto}, com o preço de ${addPreco}!`)
+}
+
+function delProduto(delProdutoPos, contProduto){ // exclui um ou mais itens dependendo da posição solicitada
+    estoque.splice(delProdutoPos, contProduto) // exclui produtos através da posição
+    preco.splice(delProdutoPos, contProduto) // exclui preços através da posição
+}
+
+// Execução
+mostrarCatalogo()
+addProduto("Sonic 3D Blast", "R$ " + "12,00")
+mostrarCatalogo()
+delProduto(4, 1)
+mostrarCatalogo()
