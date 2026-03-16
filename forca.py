e6e6ec393b5cf29b433910d29645934a597a0e02
Richard Marx <bellusci.richard@escola.pr.gov.br>
2026-03-16 09:42:37 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -1,6 +1,9 @@
 palavra = "KENNEDY"
 acertou = False
+enforcou = False
 letras_acertadas = ["_","_","_","_","_","_","_"]
+limite_tentativas = (len(palavra) + 6)
+tentativa = 1
 print(letras_acertadas)
 
 def mostrar_letras_acertadas():
@@ -9,7 +12,7 @@ def mostrar_letras_acertadas():
 
 
 print("Tente adivinhar a palavra secreta: ")
-while(not acertou):
+while(not acertou and not enforcou):
     # Mostrar letras acertadas
     mostrar_letras_acertadas()
 
@@ -21,6 +24,24 @@ while(not acertou):
              letras_acertadas[i] = letra
         i = (i + 1)
 
+    if (tentativa >= limite_tentativas):
+        print("Você perdeu :/ \nA palavra era:", palavra)
+        enforcou = True
+
     if (letras_acertadas.count("_") == 0):
         print(f"Parabéns, você encontrou a palavra secreta! A palavra é... {palavra}!")
         acertou = True
+        
+    tentativa = (tentativa + 1)
+
+
+
+# TABELA VERDADE
+# Quero fazer um suco de banana e maçã
+# banana = True
+# maca = True
+#       E         |          OU
+#  V  V  =  V     |      V  V  =  V
+#  V  F  =  F     |      V  F  =  V
+#  F  V  =  F     |      F  V  =  V
+#  F  F  =  F     |      F  F  =  F
