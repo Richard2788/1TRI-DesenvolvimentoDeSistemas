8353caeb4c6aebac91bdeddd8e9ae9fa4f2aea0d
Richard Marx <bellusci.richard@escola.pr.gov.br>
2026-03-18 08:40:39 -0300

    Add files via upload

---
+++ aula8_1.js
@@ -0,0 +1,54 @@
+// JSON: JavaScript Object Notation
+// É una notação para traduzir objetos JavaScript em texto
+
+const alunos = [
+    {
+        nome: 'Richard',
+        dataNasc: '26/07/2009',
+        turma: '3°DSC'
+    },
+    {
+        nome: 'Pietro',
+        dataNasc: '24/12/2009',
+        turma: '3°DSC'
+    },
+    {
+        nome: 'Guilherme',
+        dataNasc: '09/06/2009',
+        turma: '3°DSC'
+    },
+    {
+        nome: 'Nycolas',
+        dataNasc: '17/09/2008',
+        turma: '3°DSC'
+    }
+]
+
+const alunosJSON = JSON.stringify(alunos)
+console.log(alunos)
+console.log(alunos[0].nome)
+console.log(alunosJSON)
+
+const alunosOBJ = JSON.parse(alunosJSON)
+console.log(alunosOBJ)
+
+let notasPedro = [8.0, 4.5, 6.5, 9.0];
+function calculaMedia(notas){
+    media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length
+}
+calculaMedia(notasPedro);
+console.log(notasPedro.length)
+console.log(media)
+
+
+function criaArquivo(obj){
+    const dadosJSON = JSON.stringify(obj)
+    const fs = require('fs')
+    fs.writeFileSync('alunos_1.json', dadosJSON)
+}
+
+criaArquivo(alunos);
+
+// Lendo um arquivo JSON
+const arquivo = require('./alunos.json')
+console.log(arquivo)
