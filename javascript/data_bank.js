e623a0e56afc501c31e1057e02e8d83b7fc7065f
Richard Marx <bellusci.richard@escola.pr.gov.br>
2026-04-01 12:29:23 -0300

    Add files via upload

--- javascript/data_bank.js
+++ javascript/data_bank.js
@@ -7,11 +7,11 @@ USANDO OBJETOS, COM PELO MENOS 10 ELEMENTOS.
 SEGUIR O MODELO ABAIXO:
 */
 const modelo = {
-    titulo: "Título go Jogo",
+    titulo: "Título do Jogo",
     estudios: "Estúdios",
     ano: 9999,
-    genero: ['Gênero 1', 'Gênero 2'],
-    plataformas: ['Microsoft Windows', 'Xbox 360'],
+    generos: 'Gênero 1, Gênero 2',
+    plataformas: 'Microsoft Windows, Xbox 360',
     sinopse: "O ano é 2047. Uma enorme bola de fogo nuclear explode no céu noturno, marcando o início dramático da Terceira Guerra de Tiberium e o tão esperado retorno da franquia de estratégia em tempo real mais inovadora de todos os tempos.",
     preco: 32.99
 }
@@ -21,37 +21,37 @@ const jogos = [
         titulo: "Command and Conquer 3: Tiberium Wars",
         estudios: "Electronic Arts",
         ano: 2007,
-        generos: ['RTS (Real Time Strategy)', 'Ação', 'Estratégia', 'Sci-Fi'],
-        plataformas: ['Microsoft Windows', 'Xbox 360', 'macOS', 'Java Platform', 'Micro Edition', 'Mac OS Classic'],
+        generos: 'RTS (Real Time Strategy), Ação, Estratégia, Sci-Fi',
+        plataformas: 'Microsoft Windows, Xbox 360, macOS, Java Platform, Micro Edition, Mac OS Classic',
         sinopse: "O ano é 2047. Uma enorme bola de fogo nuclear explode no céu noturno, marcando o início dramático da Terceira Guerra de Tiberium e o tão esperado retorno da franquia de estratégia em tempo real mais inovadora de todos os tempos.",
-        precos: 20.33
+        preco: 219.99
     },
     {
         titulo: "Ace Combat 7: Skies Unknown",
-        estudios: ["Bandai Namco Entertainment", "Namco Bandai Games America Inc."],
+        estudios: "Bandai Namco Entertainment, Namco Bandai Games America Inc.",
         ano: 2019,
-        generos: ['Ação de Voo Arcade', 'Simulador de Combate Aéreo', 'Ação', 'Realidade Virtual', 'Shooter'],
-        plataformas: ['PlayStation 4', 'Nintendo Switch', 'Xbox One', 'Microsoft Windows'],
-        sinopse: "Situado no mundo de Strangereal durante a 'Segunda Guerra Continental Useana' em 2019, a trama foca no piloto 'Trigger', acusado falsamente de assassinato e transferido para o esquadrão penal 'Spare', lutando na guerra entre a Federação de Osea e o Reino de Erusea",
-        precos: 32.99
+        generos: 'Ação de Voo Arcade, Simulador de Combate Aéreo, Ação, Realidade Virtual, Shooter',
+        plataformas: 'PlayStation 4, Nintendo, Switch Xbox One, Microsoft Windows',
+        sinopse: "Situado no mundo de Strangereal durante a 'Segunda Guerra Continental Useana' em 2019, a trama foca no piloto 'Trigger', acusado falsamente de assassinato e transferido para o esquadrão penal 'Spare', lutando na guerra entre a Federação de Osea e o Reino de Erusea.",
+        preco: 149.99
     },
     {
-        titulo: "Título go Jogo",
-        estudios: "Estúdio",
-        ano: 9999,
-        generos: ['Gênero 1', 'Gênero 2'],
-        plataformas: ['Microsoft Windows', 'Xbox 360'],
-        sinopse: "O ano é 2047. Uma enorme bola de fogo nuclear explode no céu noturno, marcando o início dramático da Terceira Guerra de Tiberium e o tão esperado retorno da franquia de estratégia em tempo real mais inovadora de todos os tempos.",
-        precos: 32.99
+        titulo: "Hi-Fi Rush",
+        estudios: "Tango Gameworks",
+        ano: 2023,
+        generos: 'Plataforma, Aventura, Luta, Ação',
+        plataformas: 'PlayStation 5, Xbox Cloud Gaming, Microsoft Windows, Xbox Series X e Series S',
+        sinopse: "Chai, um aspirante a rockstar, tem seu reprodutor de música fundido ao coração após um experimento defeituoso. Marcado como 'defeito' por uma megacorporação, ele luta pela liberdade em um mundo onde tudo — plataforma, inimigos e ataques — se sincroniza ao ritmo da música",
+        preco: 149.95
     },
     {
-        titulo: "Título go Jogo",
-        estudios: "Estúdio",
+        titulo: "Título do Jogo",
+        estudios: "Estúdios",
         ano: 9999,
-        generos: ['Gênero 1', 'Gênero 2'],
-        plataformas: ['Microsoft Windows', 'Xbox 360'],
+        genero: 'Gênero 1, Gênero 2',
+        plataformas: 'Microsoft Windows, Xbox 360',
         sinopse: "O ano é 2047. Uma enorme bola de fogo nuclear explode no céu noturno, marcando o início dramático da Terceira Guerra de Tiberium e o tão esperado retorno da franquia de estratégia em tempo real mais inovadora de todos os tempos.",
-        precos: 32.99
+        preco: 32.99
     }
 ]
 
@@ -73,20 +73,21 @@ function mostrarJogos(lista){
     console.log("##### ESTOQUE DE JOGOS #####")
     lista.forEach((jogo) => {
         console.log(jogo.titulo + " - " + 
-                    jogo.estudios + " Ano:" + 
-                    jogo.ano + " Gêneros: " +
-                    jogo.generos + "Plataformas: " +
-                    jogo.plataformas + "Sinopse: "
-                    + jogo.sinopse + " Preços: R$ "+
-                    jogo.precos.toFixed(2).replaceAll(".",","))
+                    jogo.estudios + "\nAno: " + 
+                    jogo.ano + "\nGêneros: " +
+                    jogo.generos + "\nPlataformas: " +
+                    jogo.plataformas + "\nSinopse: "
+                    + jogo.sinopse + "\nPreços: R$ " +
+                    jogo.preco.toFixed(2).replaceAll(".",",") +
+                    "\n------------")
     })
 }
 mostrarJogos(arquivo)
 
-let jogosFiltrados = arquivo.filter((jogo) => jogo.preco > 100)
+let jogosFiltrados = arquivo.filter((jogo) => jogo.preco < 100)
 mostrarJogos(jogosFiltrados)
 
 let jogosDesconto = arquivo.map((jogo) => {
-    return {...jogo, preco: jogo.preco * 0.9}
+    return {...jogo, preco: jogo.preco * 0.67}
 })
 mostrarJogos(jogosDesconto)
