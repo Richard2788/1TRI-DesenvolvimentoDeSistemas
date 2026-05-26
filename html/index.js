a92a30c607991155a72a239ecbf2e70e0a5dd67b
Richard Marx <bellusci.richard@escola.pr.gov.br>
2026-05-26 09:46:18 -0300

    Add files via upload

--- html/index.js
+++ html/index.js
@@ -1,9 +1,26 @@
+// npm init
+// npm i express
+
 const express = require('express')
+const fs = require('fs')
 const app = express()
 const port = 3001
 
 app.get('/ola', (req, res) => {
     res.send('Hello World!')
+})
+
+app.post('/clientes/cadastro', (req, res) => {
+    // pegar os dados que vem do usuário
+    // salvar no bd.json
+    // resposta se tudo deu certo
+    const dados = req.body
+    const bd = fs.readFileSync('jogos.json', 'utf8')
+    const bdOBJ = JSON.parse(bd)
+    bdOBJ.push(dados)
+    fs.writeFileSync('jogos.json', JSON.stringify(bdOBJ), 'utf8')
+    console.log(bdOBJ)
+    res.json({resposta: 'ok'})
 })
 
 app.listen(port, () => {
