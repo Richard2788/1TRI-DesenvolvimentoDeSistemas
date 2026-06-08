326c54185e9c5803bae4e961227a2747a92ee15a
Richard Marx <bellusci.richard@escola.pr.gov.br>
2026-06-08 09:43:01 -0300

    Add files via upload

--- python/banco/telaGerente.py
+++ python/banco/telaGerente.py
@@ -32,52 +32,48 @@ def cadastrar():
         text= (f'Conta: {conta.numero} Titular: {conta.titular} cadastrado com sucesso!'),
         fg= 'green'
     )
+    
 def abrir_tela2(janela_pai):
     # Toplevel cria a nova janela conectada à janela principal
-    nova_tela = tk.Toplevel(janela_pai)
-    nova_tela.title("Segunda Tela")
-    nova_tela.geometry("300x200")
+    janela_pai = tk.Toplevel(janela_pai)
+    janela_pai.title("Tela Gerente")
+    janela_pai.geometry("480x360")
     
-    botao_fechar = tk.Button(nova_tela, text="Fechar", command=nova_tela.destroy)
-    botao_fechar.pack()
-    # CONFIGURAÇÕES DA TELA
-    app = tk.Tk()   # Obtém a função principal da tela e guarda na variável 'app'
-    app.title('Tela Gerente')     # Define o título da tela
-    app.geometry('480x360')     # Define a escala da tela
-
+    
     # TÍTULO
-    titulo = tk.Label(app, text= 'Cadastro de clientes', font= {'Arial', 45, })
+    titulo = tk.Label(janela_pai, text= 'Cadastro de clientes', font= {'Arial', 45, })
     titulo.pack()
 
     # TITULAR  
-    label_titular = tk.Label(app, text= 'Titular:')     # Cria um label para o campo de titular
+    label_titular = tk.Label(janela_pai, text= 'Titular:')     # Cria um label para o campo de titular
     label_titular.pack(pady= 5)    # Define o padding do label de titular
-    input_titular = tk.Entry(app)     # Cria a entrada de texto do campo de titular
+    input_titular = tk.Entry(janela_pai)     # Cria a entrada de texto do campo de titular
     input_titular.pack()  # .pack define as propriedades do campo de titular
 
-
     # AGENCIA
-    label_agencia = tk.Label(app, text= "Agência:")
+    label_agencia = tk.Label(janela_pai, text= "Agência:")
     label_agencia.pack(pady= 5)
-    input_agencia = tk.Entry(app)
+    input_agencia = tk.Entry(janela_pai)
     input_agencia.pack()
 
 
     # CPF
-    label_cpf = tk.Label(app, text= "CPF:")
+    label_cpf = tk.Label(janela_pai, text= "CPF:")
     label_cpf.pack(pady= 5)
-    input_cpf = tk.Entry(app)
+    input_cpf = tk.Entry(janela_pai)
     input_cpf.pack()
 
 
     # BOTÃO
-    botao = tk.Button(app, text= 'Enviar', command= cadastrar)   # Cria um botão para enviar os dados da inseridos nas entradas de texto, text é texto do botão e command define reações ao usar o botão, neste caso chama a função 'login'
+    botao = tk.Button(janela_pai, text= 'Enviar', command= cadastrar)   # Cria um botão para enviar os dados da inseridos nas entradas de texto, text é texto do botão e command define reações ao usar o botão, neste caso chama a função 'login'
     botao.pack()    # Define as propriedades do botão
 
 
     # RESPOSTA
-    label_res = tk.Label(app, text= '')
+    label_res = tk.Label(janela_pai, text= '')
     label_res.pack(pady= 5)
 
+    botao_fechar = tk.Button(janela_pai, text="Fechar", command=janela_pai.destroy)
+    botao_fechar.pack()
 
-    app.mainloop()  # Executa a tela em laço
+    janela_pai.mainloop()  # Executa a tela em laço
