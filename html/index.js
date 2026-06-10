f1ffdcb98729599b7d4a2a584cc0857676e9a13c
Richard Marx <bellusci.richard@escola.pr.gov.br>
2026-06-10 08:59:04 -0300

    Add files via upload

--- html/index.js
+++ html/index.js
@@ -76,6 +76,30 @@ app.delete('/clientes/:cpf', (req, res) => {
     };
 });
 
+app.post('/clientes', (req, res) => {
+    const cpf_param = req.params['cpf'];
+    const cliente = req.body;
+    if (!cliente || Object.keys(cliente).length === 0) {
+        return res.status(400).json({ resposta: "Body não preenchido" });
+    } else {
+        try {
+            const clientes = JSON.parse(fs.readFileSync('bd.json', 'utf8'));
+            const usuario_encontrado = clientes.findIndex((cliente) => {
+                return (cliente.cpf.replace(/\D/g, '')) === (cpf_param)
+            });
+            console.log(usuario_encontrado)
+            if (usuario_encontrado === -1) {
+                return res.status(404).json({ erro: 'Cliente não existe no banco de dados' });
+            };
+            const cliente = clientes[indice]
+            fs.writeFileSync('bd.json', JSON.stringify(cliente), 'utf8');
+            res.status(201).json({ resposta: "Cliente cadastrado com sucesso!" });
+        } catch (error) {
+            res.status(500).json({ resposta: error.message });
+        };
+    }
+});
+
 app.listen(port, () => {
     console.log('API executando na porta', port);
 });
