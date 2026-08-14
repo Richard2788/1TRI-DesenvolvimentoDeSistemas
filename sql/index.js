0d4cf332896ac1816a7be4c0d2d15a5a4afdea6e
Richard Marx <bellusci.richard@escola.pr.gov.br>
2026-08-14 08:58:41 -0300

    Implement client registration endpoint
    Added endpoint to register a new client with hashed password.

--- sql/index.js
+++ sql/index.js
@@ -1,4 +1,35 @@
+// npm init
+// npm i express
 const express = require('express')
 const app = express()
 const port = 3000
 app.use(express.json())
+
+// npm i mysql2
+// npm i bcrypt
+const db = require('./db')
+const bcrypt = require('bcrypt')
+
+app.post('/cliente', async (req, res) => {
+    try {
+        const cliente = req.body
+        const senhaCript = bcrypt.hashSync(cliente.senha, 10)
+        cliente.senha = senhaCript
+
+        // envio para o BD
+        const resultado = await db.pool.query(
+            `INSERT INTO cliente (
+                nome, cpf, email, celular, senha
+            ) VALUES ( ?, ?, ?, ?, ? )`,
+            [cliente.nome, cliente.cpf, cliente.email, cliente.celular, cliente.senha]
+
+        )
+        res.status(201).json({mensagem: 'Cliente cadastrado com sucesso! id = ' + resultado[0].insertId})
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+})
+
+app.listen(port, () => {
+    console.log('API rodando na porta', port)
+})
