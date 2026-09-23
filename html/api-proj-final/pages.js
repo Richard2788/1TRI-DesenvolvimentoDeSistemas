3361a3bdef71fd5170533db92b48c8f69d778ce3
Richard2788 <bellusci.richard@escola.pr.gov.br>
2026-09-23 09:11:20 -0300

    Perfil cadastro e login atualizados

---
+++ html/api-proj-final/pages.js
@@ -0,0 +1,25 @@
+function atualizar_navbar() {
+    const nome = localStorage.getItem('nome_cliente')
+    const token = localStorage.getItem('token')
+
+    const nav_login = document.getElementById("nav-login")
+    const btn_user_info = document.getElementById("user-info")
+
+    if (token && nome) {
+        nav_login.style.display = "none"
+        btn_user_info.style.display = "inline"
+        btn_user_info.innerText = "Olá, " + nome.split(" ")[0]
+    } else {
+        nav_login.style.display = "inline"
+        btn_user_info.style.display = "none"
+
+    }
+}
+
+function logout() {
+    localStorage.removeItem("nome_cliente")
+    localStorage.removeItem("token")
+    window.location.href("login.html")
+}
+
+window.addEventListener("DOMContentLoaded", atualizar_navbar)
